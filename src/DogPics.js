3c603f401cefbc7fe9874b5f4a8ad52ac503ea81
manoti <manotimike@gmail.com>
2023-04-22 00:44:48 +0300

    completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
 function DogPics() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([])
 
   useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
+    console.log('useEffect')
+    fetch('https://dog.ceo/api/breeds/image/random/3')
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
+        console.log('setState')
+        setImages(data.message)
+      })
+  }, [])
 
-  console.log("render");
+  console.log('render')
 
   return (
     <div>
@@ -23,7 +23,7 @@ function DogPics() {
         <img src={image} key={image} />
       ))}
     </div>
-  );
+  )
 }
 
-export default DogPics;
+export default DogPics
